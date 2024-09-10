63ab87c561a0c14a214739070927d4184c061769
aakash-gupta24 <aakashgupta11513@gmail.com>
2024-09-10 21:01:39 +0530

    seperated add and global
    made screen clear when user selected

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function AddUser(){
     let n=document.createElement("button")
     n.innerText=prompt("Enter the name","");
-    n.onclick=function(){loadData(h.innerText);};
-    document.getElementById("Users-side").appendChild(n);
+    //n.onclick=function(){loadData(h.innerText);};
+    document.getElementById("AllUsers").appendChild(n);
 }
 function loadData(Uname){
     document.getElementById("Chat-Screen").innerHTML=Uname;
